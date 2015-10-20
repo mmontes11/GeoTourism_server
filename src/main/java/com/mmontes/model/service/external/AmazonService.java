a5b88e063c04f108e2e93ff4bf9cb908c7f5b0ff
mmontes11 <martin11lrx@gmail.com>
2015-10-20 19:05:57 +0200

    Amazon Service refactorization

--- src/main/java/com/mmontes/model/service/external/AmazonService.java
+++ src/main/java/com/mmontes/model/service/external/AmazonService.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.apache.commons.codec.binary.Base64;
 
 import java.io.*;
 import java.util.Calendar;
@@ -15,15 +16,20 @@ import static com.mmontes.util.Constants.AMAZON_S3_ROOT_URL;
 public class AmazonService {
 
     public static String uploadFile (String name,String photoName,String fileContent) throws IOException {
+
+        String[] parts = fileContent.split("data:")[1].split(";base64,");
+        String contentType = parts[0];
+        String content = parts[1];
+
+        byte[] decodedContent = Base64.decodeBase64(content.getBytes());
+        InputStream fis = new ByteArrayInputStream(decodedContent);
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(decodedContent.length);
+        metadata.setContentType(contentType);
+        metadata.setCacheControl("public, max-age=31536000");
+
         AmazonS3 s3 = new AmazonS3Client();
         String fileName = Calendar.getInstance().getTimeInMillis() + "_" + photoName;
-
-        byte[] bI = org.apache.commons.codec.binary.Base64.decodeBase64((fileContent.substring(fileContent.indexOf(",")+1)).getBytes());
-        InputStream fis = new ByteArrayInputStream(bI);
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(bI.length);
-        metadata.setContentType("image/png");
-        metadata.setCacheControl("public, max-age=31536000");
 
         PutObjectRequest por = new PutObjectRequest(AMAZON_S3_BUCKET_NAME,fileName,fis, metadata);
         s3.putObject(por.withCannedAcl(CannedAccessControlList.PublicRead));

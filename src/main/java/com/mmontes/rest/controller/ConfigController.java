31acb58324affabc74307eb7b0cd0b4dbb0cedc6
Martin <martin11lrx@gmail.com>
2016-02-13 11:40:06 +0100

    + ctrl 4 config

--- src/main/java/com/mmontes/rest/controller/ConfigController.java
+++ src/main/java/com/mmontes/rest/controller/ConfigController.java
@@ -1,7 +1,23 @@
 package com.mmontes.rest.controller;
 
+import com.mmontes.model.service.ConfigService;
+import com.mmontes.util.dto.ConfigDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConfigController {
+
+    @Autowired
+    private ConfigService configService;
+
+    @RequestMapping(value = "/admin/config", method = RequestMethod.GET)
+    public ResponseEntity<ConfigDto>
+    getConfig(){
+        return new ResponseEntity<>(configService.getConfig(), HttpStatus.OK);
+    }
 }

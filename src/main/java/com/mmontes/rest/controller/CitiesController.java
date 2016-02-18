cb74c0b418cfbb73d7af1f7d3e56f255cd4bc3d3
Martin <martin11lrx@gmail.com>
2016-02-18 20:41:07 +0100

    Added REST op to retrieve cities from OSM

--- src/main/java/com/mmontes/rest/controller/CitiesController.java
+++ src/main/java/com/mmontes/rest/controller/CitiesController.java
@@ -2,9 +2,11 @@ package com.mmontes.rest.controller;
 
 import com.mmontes.model.service.CityService;
 import com.mmontes.util.dto.CityDto;
+import com.mmontes.util.exception.SyncException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +24,16 @@ public class CitiesController {
     findAllCities() {
         List<CityDto> cities = cityService.findAll();
         return new ResponseEntity<>(cities, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/admin/cities/sync", method = RequestMethod.POST)
+    public ResponseEntity
+    syncCities(@RequestBody List<CityDto> cities) {
+        try {
+            cityService.syncCities(cities);
+        } catch (SyncException e) {
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
